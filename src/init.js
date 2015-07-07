17ac4d969210ab6bf5c1683bec2cbd635098dcc8
MKS-students <mks.students@hackreactor.com>
2015-07-06 18:49:27 -0700

    finish pseudoclassical style

--- src/init.js
+++ src/init.js
@@ -22,12 +22,16 @@ $(document).ready(function(){
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+    dancer.setPosition(dancer.top, dancer.left);
+    dancer.step();
     $('body').append(dancer.$node);
+
   });
 });
 
